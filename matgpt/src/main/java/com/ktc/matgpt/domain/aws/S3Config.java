5e730b35e50cce9f962158baf2171491ddd28217
sk980919 <sk980919@kakao.com>
2023-11-15 16:08:36 +0900

    S3Config.java

--- matgpt/src/main/java/com/ktc/matgpt/domain/aws/S3Config.java
+++ matgpt/src/main/java/com/ktc/matgpt/domain/aws/S3Config.java
@@ -1,5 +1,6 @@
 package com.ktc.matgpt.domain.aws;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -20,6 +21,9 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
+    private static final String PROXY_HOST = "krmp-proxy.9rum.cc";
+    private static final int PROXY_PORT = 3128;
+
     @Bean
     public AmazonS3Client amazonS3Client() {
         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
@@ -28,6 +32,15 @@ public class S3Config {
                 .standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withClientConfiguration(clientConfiguration())
                 .build();
+    }
+
+    @Bean
+    public ClientConfiguration clientConfiguration() {
+        final ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setProxyHost(PROXY_HOST);
+        clientConfiguration.setProxyPort(PROXY_PORT);
+        return clientConfiguration;
     }
 }
