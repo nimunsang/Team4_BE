46f1413a1bd6d195af1311c322485453b2b134dd
Kong Heechan <sk980919@naver.com>
2023-11-16 03:02:56 +0900

    bugfix: 커서기반 페이지네이션

--- matgpt/src/main/java/com/ktc/matgpt/domain/review/ReviewRestController.java
+++ matgpt/src/main/java/com/ktc/matgpt/domain/review/ReviewRestController.java
@@ -51,9 +51,9 @@ public class ReviewRestController {
     public ResponseEntity<?> findAllByStoreId(@PathVariable Long storeId,
                                               @RequestParam(defaultValue = "latest") String sortBy,
                                               @RequestParam(required = false) Long cursorId,
-                                              @RequestParam(required = false) Integer cursor
+                                              @RequestParam(required = false) Integer cursorLikes
     ) {
-        PageResponse<?, ReviewResponse.StoreReviewDTO> responseDTO = reviewService.findPageByStoreId(storeId, sortBy, cursorId, cursor);
+        PageResponse<?, ReviewResponse.StoreReviewDTO> responseDTO = reviewService.findPageByStoreId(storeId, sortBy, cursorId, cursorLikes);
         return ResponseEntity.ok(com.ktc.matgpt.utils.ApiUtils.success(responseDTO));
     }
 
