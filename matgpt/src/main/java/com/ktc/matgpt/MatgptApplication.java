8801be52e51818104c09940d6faf4261105e597d
Kong Heechan <sk980919@naver.com>
2023-11-16 21:42:25 +0900

    bugfix: timezone 설정

--- matgpt/src/main/java/com/ktc/matgpt/MatgptApplication.java
+++ matgpt/src/main/java/com/ktc/matgpt/MatgptApplication.java
@@ -1,9 +1,12 @@
 package com.ktc.matgpt;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @EnableJpaAuditing
@@ -12,6 +15,11 @@ public class MatgptApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MatgptApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 
